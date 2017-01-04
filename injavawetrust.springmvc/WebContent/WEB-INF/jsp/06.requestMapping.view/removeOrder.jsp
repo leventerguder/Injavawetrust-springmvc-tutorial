cd134a04a3d5bd9de4a4ffb601e43d563f0d537b
leventerguder <erguder.levent@gmail.com>
2017-01-04 22:53:55 +0200

    review

--- injavawetrust.springmvc/WebContent/WEB-INF/jsp/06.requestMapping.view/removeOrder.jsp
+++ injavawetrust.springmvc/WebContent/WEB-INF/jsp/06.requestMapping.view/removeOrder.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>@Controller</title>
+<title>removeOrder.jsp</title>
 </head>
 <body>
 	<h2>${message}</h2>
--- injavawetrust.springmvc/src/_06/requestMapping/controller/OrderController.java
+++ injavawetrust.springmvc/src/_06/requestMapping/controller/OrderController.java
@@ -8,25 +8,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
 
 	@RequestMapping("/add")
+	// http://localhost:8080/injavawetrust.springmvc/order/add
 	public String add(Model model) {
-		model.addAttribute("message", "add() method");
+		model.addAttribute("message", "OrderController#add");
 		return "06.requestMapping.view/addOrder";
 	}
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	// http://localhost:8080/injavawetrust.springmvc/order/list
 	public String list(ModelMap modelMap) {
-		modelMap.addAttribute("message", "list() method");
+		modelMap.addAttribute("message", "OrderController#list");
 		return "06.requestMapping.view/getOrderList";
 	}
 
-	@RequestMapping(value = {"/remove", "/delete"}, method = RequestMethod.GET)
+	@RequestMapping(value = { "/remove", "/delete" }, method = RequestMethod.GET)
+	// http://localhost:8080/injavawetrust.springmvc/order/delete
+	// http://localhost:8080/injavawetrust.springmvc/order/remove
 	public ModelAndView remove() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("06.requestMapping.view/removeOrder");
-		mv.addObject("message", "remove() method");
+		mv.addObject("message", "OrderController#remove");
 		return mv;
 	}
 }
