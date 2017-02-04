a5f42567fbaaeb2a64496b2173d6c7a5181ba02e
leventerguder <erguder.levent@gmail.com>
2017-02-04 08:27:20 +0200

    review

--- injavawetrust.springmvc/src/_16/xmlViewResolver/controller/ResolverController.java
+++ injavawetrust.springmvc/src/_16/xmlViewResolver/controller/ResolverController.java
@@ -1,4 +1,4 @@
-package _13.xmlViewResolver.controller;
+package _16.xmlViewResolver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
