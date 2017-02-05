34719e352aa5139c45a8e96c81e521283b39433f
leventerguder <erguder.levent@gmail.com>
2017-02-05 09:01:40 +0300

    review

--- injavawetrust.springmvc/src/_18/internalResourceViewResolver/controller/ResolverController3.java
+++ injavawetrust.springmvc/src/_18/internalResourceViewResolver/controller/ResolverController3.java
@@ -1,4 +1,4 @@
-package _15.internalResourceViewResolver.controller;
+package _18.internalResourceViewResolver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +8,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class ResolverController3 {
 
 	@RequestMapping("/page5")
+	// http://localhost:8080/injavawetrust.springmvc/page5
 	public ModelAndView handleViewResolverForPage5() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("message", "page5");
-		modelAndView.setViewName("15.internalResourceViewResolver.view/myPage5");
+		modelAndView.setViewName("18.internalResourceViewResolver.view/myPage5");
 		return modelAndView;
 	}
 
 	@RequestMapping("/page6")
+	// http://localhost:8080/injavawetrust.springmvc/page5
 	public ModelAndView handleViewResolverForPage6() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.addObject("message", "page6");
-		modelAndView.setViewName("15.internalResourceViewResolver.view/myPage6");
+		modelAndView.setViewName("18.internalResourceViewResolver.view/myPage6");
 		return modelAndView;
 	}
 }
